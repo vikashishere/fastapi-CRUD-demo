ffda98288c03bce16c5125957c6b638243f482b6
Vikash <vikashdas770@gmail.com>
2024-05-05 17:11:18 +0530

    main.py file completed and updated

--- main.py
+++ main.py
@@ -13,3 +13,21 @@ employees = {
         "designation": "SDE"
     }
 }
+
+@app.get("/")
+def home():
+    return "Welcome to the FastAPI demonstration :-)"
+
+@app.get("/employees")
+def get_employees():
+    return employees
+
+@app.get("/employee/{emp_id}")
+def get_employee(emp_id: int):
+    if emp_id not in employees:
+        return f"No employee exist with ID: {emp_id}"
+    return employees[emp_id]
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
